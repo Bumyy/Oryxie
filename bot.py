aafc0001dae84a94ba65d639728b9391f1aae9d8
Bumyy <57840717+Bumyy@users.noreply.github.com>
2025-11-07 03:36:25 +0100

    remove special_events cog

--- bot.py
+++ bot.py
@@ -98,7 +98,7 @@ class MyBot(commands.Bot):
             await self.load_extension('cogs.cargo_training')
             await self.load_extension('cogs.utils')
             await self.load_extension('cogs.flight_generator_pdf')
-            await self.load_extension('cogs.special_events')
+            #await self.load_extension('cogs.special_events')
             await self.load_extension('cogs.shop_cog')
             await self.load_extension('cogs.mission')
             await self.load_extension('cogs.gate_assignment')
