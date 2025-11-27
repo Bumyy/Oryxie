7fde0cc3812dcd8768b2eb5f9313d8999132db03
Ayush <anandayush963@gmail.com>
2025-11-27 13:06:22 +0530

    updated bot.py for flight poll system

--- bot.py
+++ bot.py
@@ -103,6 +103,7 @@ class MyBot(commands.Bot):
             await self.load_extension('cogs.mission')
             await self.load_extension('cogs.gate_assignment')
             await self.load_extension('cogs.callsign_finder')
+            await self.load_extension('cogs.flight_poll_system')
             
             #await self.load_extension('cogs.live_flights')
             #await self.load_extension('cogs.remainder')
